57fed05246162ec09827f09b3c3c6f81ed29f1c0
ZNFNDMN <ramin.franco@gmail.com>
2025-08-14 13:00:39 +0400

    package tools créé et fonctionnel

---
+++ game_dev_tools/__init__.py
@@ -0,0 +1,1 @@
+from game_dev_tools import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="game_dev_tools",
+    version="1.0.0",
+    packages=find_packages(),
+    author="ZNFNDMN",
+    description="Outils en python pour le game dev"
+)
