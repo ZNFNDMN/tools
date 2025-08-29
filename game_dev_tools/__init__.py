9192d2ad45bad755d5182838ebed5f30fd2b2aba
ZNFNDMN <ramin.franco@gmail.com>
2025-08-29 14:58:32 +0400

    modif

--- game_dev_tools/__init__.py
+++ game_dev_tools/__init__.py
@@ -7,7 +7,8 @@ from .game_dev_tools import draw_grid
 from .game_dev_tools import draw_dots
 from .game_dev_tools import draw_coordinate_fraction
 from .game_dev_tools import create_surface
+from .game_dev_tools import keep_circle_on_screen
 from .game_dev_tools import show_surface
 from .game_dev_tools import show_coordinate
 
-#__all__ = ["circles_collide", "get_collision_point_of_circles","get_collision_point_angle","draw_rect", "draw_grid", "draw_dots", "draw_coordinate_fraction", "float_range","create_surface","show_coordinate","show_surface"]
+#__all__ = ["circles_collide", "get_collision_point_of_circles","get_collision_point_angle","draw_rect", "draw_grid", "draw_dots", "draw_coordinate_fraction", "float_range","create_surface","show_coordinate","show_surface", "keep_circle_on_screen"]
